62923aabd681ef53e6996a50a62af58bcf28311b
akobartek <sokolowskijbartek@gmail.com>
2024-06-12 00:12:53 +0200

    unused import removed

--- composeApp/src/commonMain/kotlin/pl/kapucyni/wolczyn/app/shop/presentation/composables/ProductPhotosPager.kt
+++ composeApp/src/commonMain/kotlin/pl/kapucyni/wolczyn/app/shop/presentation/composables/ProductPhotosPager.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
