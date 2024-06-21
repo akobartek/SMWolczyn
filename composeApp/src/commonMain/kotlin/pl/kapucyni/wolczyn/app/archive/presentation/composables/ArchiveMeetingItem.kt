a224b4debf529a6abb9f6f6f667f3bc01347c555
akobartek <sokolowskijbartek@gmail.com>
2024-06-21 17:03:44 +0200

    archive ui fixed

--- composeApp/src/commonMain/kotlin/pl/kapucyni/wolczyn/app/archive/presentation/composables/ArchiveMeetingItem.kt
+++ composeApp/src/commonMain/kotlin/pl/kapucyni/wolczyn/app/archive/presentation/composables/ArchiveMeetingItem.kt
@@ -1,17 +1,25 @@
 package pl.kapucyni.wolczyn.app.archive.presentation.composables
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
 import pl.kapucyni.wolczyn.app.archive.domain.model.ArchiveMeeting
+import pl.kapucyni.wolczyn.app.common.presentation.composables.WolczynText
 
 @Composable
 fun ArchiveMeetingItem(meeting: ArchiveMeeting) {
@@ -21,12 +29,25 @@ fun ArchiveMeetingItem(meeting: ArchiveMeeting) {
     ) {
         Box(modifier = Modifier
             .height(200.dp)
-            .fillMaxWidth()) {
+            .fillMaxWidth()
+        ) {
             AsyncImage(
                 model = meeting.photoUrl,
                 contentDescription = null,
                 contentScale = ContentScale.Crop,
                 modifier = Modifier.fillMaxSize()
+            )
+            WolczynText(
+                text = meeting.name,
+                textStyle = TextStyle(
+                    color = Color.White,
+                    fontWeight = FontWeight.Medium,
+                    fontSize = 18.sp,
+                ),
+                modifier = Modifier
+                    .align(Alignment.BottomStart)
+                    .background(color = Color(0xCC000000))
+                    .padding(6.dp)
             )
         }
     }
