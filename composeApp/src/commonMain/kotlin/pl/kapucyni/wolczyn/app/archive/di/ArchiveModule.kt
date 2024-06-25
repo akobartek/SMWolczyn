9d960a5047866a7a4255e6601f6702afb99985a5
akobartek <sokolowskijbartek@gmail.com>
2024-06-25 23:25:50 +0200

    schedule, shop, archive and weather architecture changes

--- composeApp/src/commonMain/kotlin/pl/kapucyni/wolczyn/app/archive/di/ArchiveModule.kt
+++ composeApp/src/commonMain/kotlin/pl/kapucyni/wolczyn/app/archive/di/ArchiveModule.kt
@@ -2,6 +2,7 @@ package pl.kapucyni.wolczyn.app.archive.di
 
 import org.koin.dsl.module
 import pl.kapucyni.wolczyn.app.archive.data.repository.ArchiveRepositoryImpl
+import pl.kapucyni.wolczyn.app.archive.data.sources.FirestoreArchiveSource
 import pl.kapucyni.wolczyn.app.archive.domain.repository.ArchiveRepository
 import pl.kapucyni.wolczyn.app.archive.domain.usecases.GetArchiveMeetingByNumberUseCase
 import pl.kapucyni.wolczyn.app.archive.domain.usecases.GetArchiveUseCase
@@ -9,7 +10,8 @@ import pl.kapucyni.wolczyn.app.archive.presentation.ArchiveMeetingViewModel
 import pl.kapucyni.wolczyn.app.archive.presentation.ArchiveViewModel
 
 val archiveModule = module {
-    single<ArchiveRepository> { ArchiveRepositoryImpl() }
+    single { FirestoreArchiveSource() }
+    single<ArchiveRepository> { ArchiveRepositoryImpl(get()) }
     single { GetArchiveUseCase(get()) }
     single { GetArchiveMeetingByNumberUseCase(get()) }
 
