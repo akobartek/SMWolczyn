05bcadf2015a323980dbaba56d2d38de6b344924
akobartek <sokolowskijbartek@gmail.com>
2026-03-09 00:13:26 +0100

    migrate material icons

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -58,7 +58,6 @@ kotlin {
             implementation(compose.foundation)
             implementation(compose.material)
             implementation(compose.material3)
-            implementation(compose.materialIconsExtended)
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
