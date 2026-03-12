0f41171c361b122b795f66162992958f07f71ac3
akobartek <sokolowskijbartek@gmail.com>
2026-03-12 02:10:11 +0100

    safety navigation

--- composeApp/src/commonMain/kotlin/pl/kapucyni/wolczyn/app/common/utils/NavigationUtils.kt
+++ composeApp/src/commonMain/kotlin/pl/kapucyni/wolczyn/app/common/utils/NavigationUtils.kt
@@ -1,9 +1,12 @@
 package pl.kapucyni.wolczyn.app.common.utils
 
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.NavHostController
 import pl.kapucyni.wolczyn.app.common.presentation.Screen
 
-fun NavHostController.navigateSafely(route: Screen, popUpTo: Screen? = null) =
+fun NavHostController.navigateSafely(route: Screen, popUpTo: Screen? = null) {
+    if (currentBackStackEntry?.lifecycle?.currentState != Lifecycle.State.RESUMED)
+        return
     navigate(route) {
         launchSingleTop = true
         restoreState = true
@@ -12,6 +15,7 @@ fun NavHostController.navigateSafely(route: Screen, popUpTo: Screen? = null) =
             popUpTo(it) { inclusive = true }
         }
     }
+}
 
 fun NavHostController.navigateUpSafely(source: Screen) =
     if (currentDestination?.route?.contains(source::class.simpleName ?: "") == true)
