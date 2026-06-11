88315990b1945730a3bac5c205a8d0d2355794b8
akobartek <sokolowskijbartek@gmail.com>
2026-06-12 00:41:28 +0200

    app version bumped - 2026.8 (57)

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import java.util.Properties
 
-private val appVersion = "2026.7"
+private val appVersion = "2026.8"
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
@@ -121,7 +121,7 @@ android {
         applicationId = "pl.kapucyni.wolczyn.app"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 56
+        versionCode = 57
         versionName = appVersion
     }
     packaging {
